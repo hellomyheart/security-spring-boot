0e781535a96575ccac67d4fc6ab323893dc3b705
stephen shen <723809534@qq.com>
2020-10-09 09:20:34 +0800

    csrf解决方式一

--- src/main/java/cn/hellomyheart/security/spring/boot/config/WebSecurityConfig.java
+++ src/main/java/cn/hellomyheart/security/spring/boot/config/WebSecurityConfig.java
@@ -42,13 +42,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
+        httpSecurity.csrf().disable(). //屏蔽csrf的方式一
+                authorizeRequests()
                 .antMatchers("/r/r1").hasAuthority("p1") //权限设置
                 .antMatchers("/r/r2").hasAuthority("p2")
                 .antMatchers("/r/**").authenticated() //所有/r/**,必须认证
                 .anyRequest().permitAll()  //其他请求可以访问
                 .and()
                 .formLogin() //允许表单登录
+                .loginPage("/login-view")//登录页面
+                .loginProcessingUrl("/login")//指定登录处理的url
                 .successForwardUrl("/login-success"); //自定义登录成功的页面地址
 
     }
